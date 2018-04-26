fce3bf2016757f8ce3d03b579c7cf5fcac5f8dc7
Mayuri Kadam <mayuri18kadam@gmail.com>
2018-04-25 21:56:26 -0400

    done q2

--- Assignment6/Question2.2.1New.py
+++ Assignment6/Question2.2.1New.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 import matplotlib
 import math
 
-# func: to compute midpoints for a feature column
 def calMidpoints(feature) :
     featureUnique = np.unique(feature)
     featureUnique.sort()
@@ -19,38 +18,34 @@ def calMidpoints(feature) :
     midpointsFinal = np.unique(midpoints)
     return midpointsFinal
 
-# func: calculates best IG for a given feature and set of midpoints
 def calBestThreshold(feature, classCol, midpoints, instWgt) :
-    # print("midpoints = ",len(midpoints))
     feature1 = np.vstack((feature, classCol)).T
     feature1.sort(0)
     errorDiff = -1
     mid = -1
     for m in midpoints:
         error = 0
+        rownum = 0
         for r in feature1:
-            if (r[0]<m and r[1]==-1) or (r[0]>=m and r[1]==1):
-                error = error+instWgt
+            if (r[0]<m and r[1]<=0) or (r[0]>=m and r[1]>0):
+                error = error+instWgt[rownum]
+            rownum = rownum + 1
         if abs(0.5 - error) > errorDiff :
             errorDiff = abs(0.5 - error)
             mid = m
-    # print("errorDiff = ", errorDiff)
-    # print("mid = ", mid)
     return errorDiff, mid
 
-def getBestFeatureMidpoint(dataset_train_zscore, instWgt):
+def getBestFeatureMidpoint(xTrain, yTrain, instWgt):
     finalErrorDiff = -1
     finalMidpoint = -1
     finalFeature = -1
-    for f in range(len(dataset_train_zscore[0])-1):
-        midpoints = calMidpoints(dataset_train_zscore[:,f])
-        errorDiff, midpoint = calBestThreshold(dataset_train_zscore[:, f], dataset_train_zscore[:,-1], midpoints, instWgt)
+    for f in range(len(xTrain[0])):
+        midpoints = calMidpoints(xTrain[:,f])
+        errorDiff, midpoint = calBestThreshold(xTrain[:, f], yTrain, midpoints, instWgt)
         if errorDiff > finalErrorDiff:
             finalErrorDiff = errorDiff
             finalMidpoint = midpoint
             finalFeature = f
-    # print("finalFeature = ",finalFeature)
-    # print("finalMidpoint = ", finalMidpoint)
     return finalFeature, finalMidpoint
 
 def calZScore(dataset, meanArr, stdArr):
@@ -59,39 +54,47 @@ def calZScore(dataset, meanArr, stdArr):
             dataset[j, i] = (dataset[j, i] - meanArr[i]) / stdArr[i]
     return dataset
 
-def getError(dataset_train_zscore, featureIdx, threshold, instWgt):
+def getError(y_pred, y_actual, instWgt):
+    error = 0
+    for n in range(len(y_pred)):
+        if y_pred[n] != y_actual[n]:
+            error = error + instWgt[n]
+    return error
+
+def calAccuracyScore(y_pred, y_actual):
+    return sum(y_pred != y_actual) / float(len(y_actual))
+
+def calPredictedOutput(X, featureIdx, threshold):
     y_pred = []
-    for row in dataset_train_zscore:
+    for row in X:
         if row[featureIdx]<threshold :
             y_pred.append(1)
         else:
             y_pred.append(-1)
-    error = 0
-    for n in range(len(y_pred)):
-        if y_pred[n] != dataset_train_zscore[n,-1]:
-            error = error + instWgt
-    return error, y_pred
+    return y_pred
 
 def getNormalizationConst(y_actual, y_pred, instWgt, beta):
     sum = 0
-    loss = 0
     for n in range(len(y_actual)):
         expForZ = math.exp(-1 * beta * y_actual[n] * y_pred[n])
-        expForLoss = math.exp(-1 * y_actual[n] * y_pred[n])
-        loss = loss + expForLoss*expForZ
-        sum = sum + instWgt*expForZ
-    return sum, loss
+        sum = sum + instWgt[n]*expForZ
+    return sum
 
-def calEdge(dataset_train_zscore, featureIdx, threshold, instWgt):
+def calEdge(xTrain, yTrain, featureIdx, threshold, instWgt):
     edge = 0
-    for n in range(len(dataset_train_zscore)):
-        actualOutput = 1 if dataset_train_zscore[n, featureIdx] < threshold else -1
-        edge = edge + instWgt*(dataset_train_zscore[n,-1]*actualOutput)
+    for n in range(len(xTrain)):
+        actualOutput = 1 if xTrain[n, featureIdx] < threshold else -1
+        edge = edge + instWgt[n]*(yTrain[n]*actualOutput)
     return edge
 
-def evaluate_algorithm(dataset, T, tol):
-    # print("T = ",T)
-    # print("range of T = ", np.arange(1, T+1, 1))
+def getFinalPrediction(xTest, alphaArr, featureIdxArr, thresholdArr):
+    y_predTrain = np.zeros(len(xTest))
+    for n in range(len(alphaArr)):
+        temp = [float(alphaArr[n])*int(x) for x in calPredictedOutput(xTest, featureIdxArr[n], thresholdArr[n])]
+        y_predTrain = y_predTrain + temp
+    return np.sign(y_predTrain)
+
+def evaluate_algorithm(dataset, T):
     dataset_train, dataset_test = train_test_split(dataset, test_size=0.20, random_state=42)
     meanArr = np.mean(dataset_train, axis=0)
     stdArr = np.std(dataset_train, axis=0)
@@ -100,38 +103,45 @@ def evaluate_algorithm(dataset, T, tol):
     xTrain, yTrain = dataset_train_zscore[:,0:-1], dataset_train_zscore[:,-1]
     xTest, yTest = dataset_test_zscore[:, 0:-1], dataset_test_zscore[:, -1]
 
-    instWgt = (1/len(dataset_train_zscore))
+    # Initialize
+    instWgt = np.ones(len(xTrain)) / len(xTrain)
+    trainErr = []
+    testErr = []
     alphaArr = []
     featureIdxArr = []
     thresholdArr = []
-    lossOld = 0
+
     for t in np.arange(1, T+1, 1):
         print("t = ",t)
-        print("instWgt = ",instWgt)
-        featureIdx, threshold = getBestFeatureMidpoint(dataset_train_zscore, instWgt)
+        # fit a simple decision tree, compute error on that
+        featureIdx, threshold = getBestFeatureMidpoint(xTrain, yTrain, instWgt)
+        y_predTrain = calPredictedOutput(xTrain, featureIdx, threshold)
+        y_predTest = calPredictedOutput(xTest, featureIdx, threshold)
+        trainErr1 = getError(y_predTrain, yTrain, instWgt)
+        testErr1 = getError(y_predTest, yTest, instWgt)
+        trainErr.append(trainErr1)
+        testErr.append(testErr1)
         featureIdxArr.append(featureIdx)
         thresholdArr.append(threshold)
-        edge = calEdge(dataset_train_zscore, featureIdx, threshold, instWgt)
-        print("edge = ",edge)
+
+        edge = calEdge(xTrain, yTrain, featureIdx, threshold, instWgt)
         alpha = math.log1p(abs((1+edge)/(1-edge)))
         alphaArr.append(alpha)
-        error, y_pred = getError(dataset_train_zscore, featureIdx, threshold, instWgt)
-        z, lossNew = getNormalizationConst(dataset_train_zscore[:, -1], y_pred, instWgt, alpha)
-        instWgt = (instWgt * math.exp(-1 * alpha * np.sum(np.dot(y_pred, dataset_train_zscore[:,-1]))))/z
-        print("lossNew = ",lossNew)
-        print("lossOld = ",lossOld)
-        print("abs(lossNew - lossOld) = ",abs(lossNew - lossOld))
-        if abs(lossNew - lossOld) <= tol:
-            break;
-        lossOld = lossNew
+        z = getNormalizationConst(yTrain, y_predTrain, instWgt, alpha)
+        instWgt = np.multiply(instWgt, np.exp(-1 * alpha * np.exp(-1 * alpha * np.prod((y_predTrain, yTrain), axis=0))))/z
 
+    y_test_final = getFinalPrediction(xTest, alphaArr, featureIdxArr, thresholdArr)
+    print("y_test_final = ",y_test_final)
     print("alphaArr = ",alphaArr)
     print("featureIdxArr = ", featureIdxArr)
     print("thresholdArr = ", thresholdArr)
+    print("trainErr = ", trainErr)
+    print("testErr = ", testErr)
+    print("Accuracy Score = ",calAccuracyScore(y_test_final, yTest))
 
 def main():
     print("breastcancer data:")
     breastcancer = pd.read_csv('breastcancer.csv', sep=',', header=None)
-    evaluate_algorithm(breastcancer.values, 25, 0.0001)
+    evaluate_algorithm(breastcancer.values, 25)
 
 if __name__ == "__main__": main()
